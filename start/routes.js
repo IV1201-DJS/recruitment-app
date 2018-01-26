f7d052180359cd75b7423dda7f8e561d1e399e7e
Samuel <smartin@kth.se>
2018-01-26 18:24:47 +0100

    Implement first basic graphql queries

--- start/routes.js
+++ start/routes.js
@@ -14,7 +14,20 @@
 */
 
 const Route = use('Route')
+const GraphqlAdonis = use('ApolloServer')
+const schema = require('../app/data/schema');
 
 Route.get('/', ({ request }) => {
   return { greeting: 'Hello world in JSON' }
+})
+
+Route.route('/graphql', ({ request, auth, response }) => {
+  return GraphqlAdonis.graphql({
+    schema,
+    //context: { auth }
+  }, request, response)
+}, ['GET', 'POST'])
+
+Route.get('/graphiql', ({ request, response }) => {
+  return GraphqlAdonis.graphiql({ endpointURL: '/graphql' }, request, response)
 })
