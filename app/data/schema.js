8b2b765e8b1d0a4069ddd32ffcb8880f0e04260f
Joel Wachsler <joel@wachsler.se>
2018-02-06 14:06:30 +0100

    Extract types

--- app/data/schema.js
+++ app/data/schema.js
@@ -2,65 +2,9 @@
 
 const { makeExecutableSchema } = require('graphql-tools')
 const resolvers = require('./resolvers')
+const fs = require('fs')
 
 // Define our schema using the GraphQL schema language
-const typeDefs = `
-  type User {
-    id: ID
-    username: String
-    email: String
-    firstname: String
-    lastname: String
-    ssn: String
-    role: Role
-    availabilities: [Availability]
-    competences: [UserCompetence]
-  }
-  type Role {
-    id: ID
-    name: String
-  }
-  type Availability {
-    user: User
-    from: String
-    to: String
-  }
-  type UserCompetence {
-    id: ID
-    name: String
-    experience_years: Float
-  }
-  type Competence {
-    id: ID
-    name: String
-  }
-  type RoleTranslation {
-    role: Role
-    language: Language
-    translation: String
-  }
-  type CompetenceTranslation {
-    competence: Competence
-    language: Language
-    translation: String
-  }
-  type Language {
-    id: ID
-    name: String
-  }
-  type Period {
-    from: String
-    to: String
-  }
-  type Query {
-    User(id: ID): User
-    Applications(competence_id: ID): [User]
-    Competences(name: String): [Competence]
-    CurrentUser: User
-  }
-  type Mutation {
-    createUser(username: String!, password: String!, email: String!, firstname: String!, lastname: String!, ssn: String!): User
-  }
-`
+const typeDefs = fs.readFileSync('app/data/types.gql', { encoding: 'utf8' })
 
 module.exports = makeExecutableSchema({ typeDefs, resolvers })
