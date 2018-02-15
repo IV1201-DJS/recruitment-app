771b8879c15f2a6acf72af59c5d2b82e3a8f723e
darosbanda <daros@kth.se>
2018-02-15 18:55:09 +0100

    Add sinon dependency

--- test/unit/user.spec.js
+++ test/unit/user.spec.js
@@ -2,9 +2,14 @@
 
 const { trait, test } = use('Test/Suite')('User')
 const UserMigrator = use('App/Services/UserMigrator')
+const sinon = require('sinon')
+const LegacyDBHandler = use('App/Services/LegacyDatabaseHandler')
+const Database = use('Database')
 
 trait((suite) => {
-  const migrator = new UserMigrator()
+  const legacyDBMock = sinon.mock(new LegacyDBHandler())
+  const newDBMock = sinon.mock(Database)
+  const migrator = new UserMigrator(legacyDBMock, newDBMock)
   const incompleteLegacyUser = {
     name: 'name',
     surname: 'surname',
@@ -19,14 +24,23 @@ trait((suite) => {
   suite.Context.getter('migrator', () => migrator)
   suite.Context.getter('incompleteLegacyUser', () => incompleteLegacyUser)
   suite.Context.getter('completeLegacyUser', () => completeLegacyUser)
+  suite.Context.getter('legacyDBMock', () => legacyDBMock)
+  suite.Context.getter('newDBMock', () => newDBMock)
 })
 
-test('make sure incomplete user is invalidated', async ({ assert, migrator, incompleteLegacyUser, completeLegacyUser }) => {
+test('make sure isCompleteUser() returns false for an incomplete user', async ({ assert, migrator, incompleteLegacyUser, completeLegacyUser }) => {
   const isValidUser = await migrator.isCompleteUser(incompleteLegacyUser)
   assert.equal(isValidUser, false)
 })
 
-test('make sure complete user is validated', async ({ assert, migrator, incompleteLegacyUser, completeLegacyUser}) => {
+test('make sure isCompleteUser() returns true for a complete user', async ({ assert, migrator, incompleteLegacyUser, completeLegacyUser}) => {
   const isValidUser = await migrator.isCompleteUser(completeLegacyUser)
   assert.equal(isValidUser, true)
 })
+
+// // test('make sure migrate() calls newDB.transaction', async ({ newDBMock, assert, migrator, completeLegacyUser }) => {
+// //   const expectation = newDBMock.expects('transaction')
+// //   await migrator.migrate(completeLegacyUser)
+// //   newDBMock.transaction.verify()
+// })
+
