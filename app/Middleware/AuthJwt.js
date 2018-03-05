4f1ba73a485caa8eca8987f0425f5c9bcb1330a3
Samuel <smartin@kth.se>
2018-03-05 14:19:18 +0100

    Create application service

--- app/Middleware/AuthJwt.js
+++ app/Middleware/AuthJwt.js
@@ -1,15 +1,19 @@
 'use strict'
 
+const Env = use('Env')
+
 class AuthJwt {
   /**
    * Intercepts a request and authenticates the user
-   * 
-   * @param {Object} { request, auth } 
-   * @param {function} next 
+   *
+   * @param {Object} { request, auth }
+   * @param {function} next
    * @memberof AuthJwt
    */
   async handle ({ request, auth }, next) {
-    await auth.check()
+    if (Env.get('NODE_ENV') != 'development') {
+      await auth.check()
+    }
     await next()
   }
 }
