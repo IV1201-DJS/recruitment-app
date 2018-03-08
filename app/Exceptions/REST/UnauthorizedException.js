0a32fbbef5be16bc28ec6a0a5808a1c8cf84e42b
Samuel <smartin@kth.se>
2018-03-08 15:41:05 +0100

    Log less

--- app/Exceptions/REST/UnauthorizedException.js
+++ app/Exceptions/REST/UnauthorizedException.js
@@ -1,9 +1,11 @@
 'use strict'
 
 const { LogicalException } = require('@adonisjs/generic-exceptions')
+const Logger = use('Logger')
 
 class UnauthorizedException extends LogicalException {
   handle ({ message }, { response }) {
+    Logger.error(message)
     response
       .status(401)
       .json({ error: message })
--- app/Exceptions/REST/UnprocessableException.js
+++ app/Exceptions/REST/UnprocessableException.js
@@ -1,9 +1,11 @@
 'use strict'
 
 const { LogicalException } = require('@adonisjs/generic-exceptions')
+const Logger = use('Logger')
 
 class UnprocessableException extends LogicalException {
   handle ({ message }, { response }) {
+    Logger.error(message)
     response
       .status(422)
       .json({ error: message })
